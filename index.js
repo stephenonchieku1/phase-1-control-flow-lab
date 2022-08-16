fb85f73e48a17b599412c7d6320fc26a800b2acb
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-16 14:57:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+  function scuberGreetingForFeet(distance) {
+    // Write your code here!
+    if (distance <= 400) {
+      return ('This one is on me!');
+    } else if (distance > 2000 && distance < 2500) {
+      return ('I will gladly take your thirty bucks.');
+    } else if (distance > 2500) {
+      return ('No can do.');
+    }
+  }
+  function ternaryCheckCity(city) {
+    // Write your code here!
+   return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+  }
+  
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+      case 'generous':
+      return ('Thank you so much.');
+      case 'not as generous':
+        return ('Thank you.');
+        default:
+          return ('Bye.');
+    }
+  }
